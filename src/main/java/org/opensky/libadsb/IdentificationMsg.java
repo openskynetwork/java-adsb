7c4f90789ede736fdbd1161f70f46cdc273a7c89
Markus Fuchs <fuchs@sero-systems.de>
2015-01-14 18:37:44 +0100

    Fix: IdentificationMsg categoryDescription

--- src/main/java/org/opensky/libadsb/IdentificationMsg.java
+++ src/main/java/org/opensky/libadsb/IdentificationMsg.java
@@ -146,7 +146,7 @@ public class IdentificationMsg extends ExtendedSquitter implements Serializable 
 			"Reserved"
 		}};
 		
-		return categories[getFormatTypeCode()-4][emitter_category];
+		return categories[4-getFormatTypeCode()][emitter_category];
 	}
 
 	/* (non-Javadoc)
