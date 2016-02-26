687882538647a8459f7b73d5ea2e095c265e555c
Matthias Schäfer <schaefer@cs.uni-kl.de>
2016-02-26 17:33:00 +0100

    Added decoder for all-call replies

---
+++ src/main/java/org/opensky/libadsb/msgs/AllCallReply.java
@@ -0,0 +1,82 @@
+package org.opensky.libadsb.msgs;
+
+import java.io.Serializable;
+
+import org.opensky.libadsb.tools;
+import org.opensky.libadsb.exceptions.BadFormatException;
+
+/**
+ *  This file is part of org.opensky.libadsb.
+ *
+ *  org.opensky.libadsb is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  org.opensky.libadsb is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with org.opensky.libadsb.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Decoder for Mode S extended squitters
+ * @author Matthias Schäfer <schaefer@sero-systems.de>
+ */
+public class AllCallReply extends ModeSReply implements Serializable {
+
+	private static final long serialVersionUID = -1156158096293306435L;
+	
+	private byte capabilities;
+	private byte[] interrogator; // 3 bytes
+	
+	/**
+	 * @param raw_message raw all-call reply as hex string
+	 * @throws BadFormatException if message is not all-call reply or 
+	 * contains wrong values.
+	 */
+	public AllCallReply(String raw_message) throws BadFormatException {
+		super(raw_message);
+		setType(subtype.ALL_CALL_REPLY);
+		
+		if (getDownlinkFormat() != 11) {
+			throw new BadFormatException("Message is not an all-call reply!", raw_message);
+		}
+		
+		capabilities = getFirstField();
+		
+		// extract interrogator ID
+		interrogator = getParity();
+		byte[] crc = calcParity();
+		for (int i=0; i<3; i++)
+			interrogator[i] = (byte)(0xff & ((int)interrogator[i]^(int)crc[i]));
+	}
+
+	/**
+	 * @return The emitter's capabilities (see ICAO Annex 10 V4)
+	 */
+	public byte getCapabilities() {
+		return capabilities;
+	}
+
+	/**
+	 * Some receivers already subtract the crc checksum
+	 * from the parity field right after reception.
+	 * In that case, use {@link #getParity() getParity} to get the interrogator ID.
+	 * @return the interrogator ID as a 3-byte array
+	 */
+	public byte[] getInterrogatorID() {
+		return interrogator;
+	}
+	
+	public String toString() {
+		return super.toString()+"\n"+
+				"Extended Squitter:\n"+
+				"\tCapabilities:\t\t"+getCapabilities()+"\n"+
+				"\tInterrogator:\t\t"+tools.toHexString(getInterrogatorID());
+	}
+
+}
