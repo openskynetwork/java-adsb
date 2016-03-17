2002765a5be5691969abccd7b05b92c647a533c1
Matthias Schäfer <schaefer@cs.uni-kl.de>
2016-03-17 21:52:02 +0100

    Updated generic decoder to use copy constructors for better efficiency

--- src/main/java/org/opensky/libadsb/Decoder.java
+++ src/main/java/org/opensky/libadsb/Decoder.java
@@ -1,5 +1,7 @@
 package org.opensky.libadsb;
 
+import org.opensky.libadsb.exceptions.BadFormatException;
+import org.opensky.libadsb.exceptions.UnspecifiedFormatError;
 import org.opensky.libadsb.msgs.AirbornePositionMsg;
 import org.opensky.libadsb.msgs.AirspeedHeadingMsg;
 import org.opensky.libadsb.msgs.AllCallReply;
@@ -44,66 +46,68 @@ public class Decoder {
 	 * check the message type and then cast to the appropriate class.
 	 * @param raw_message the Mode S message in hex representation
 	 * @return an instance of the most specialized ModeSReply possible
+	 * @throws UnspecifiedFormatError if format is not specified
+	 * @throws BadFormatException if format contains error
 	 */
-	public static ModeSReply genericDecoder (String raw_message) throws Exception {
+	public static ModeSReply genericDecoder (String raw_message) throws BadFormatException, UnspecifiedFormatError {
 		ModeSReply modes = new ModeSReply(raw_message);
 		
 		if (modes.getDownlinkFormat() == 4) {
-			return new AltitudeReply(raw_message);
+			return new AltitudeReply(modes);
 		}
 		else if (modes.getDownlinkFormat() == 5) {
-			return new IdentifyReply(raw_message);
+			return new IdentifyReply(modes);
 		}
 		else if (modes.getDownlinkFormat() == 11) {
-			return new AllCallReply(raw_message);
+			return new AllCallReply(modes);
 		}
 		else if (modes.getDownlinkFormat() == 17 || modes.getDownlinkFormat() == 18) {
-			ExtendedSquitter es1090 = new ExtendedSquitter(raw_message);
+			ExtendedSquitter es1090 = new ExtendedSquitter(modes);
 
 			// what kind of extended squitter?
 			byte ftc = es1090.getFormatTypeCode();
 
 			if (ftc >= 1 && ftc <= 4) // identification message
-				return new IdentificationMsg(raw_message);
+				return new IdentificationMsg(es1090);
 
 			if (ftc >= 5 && ftc <= 8) // surface position message
-				return new SurfacePositionMsg(raw_message);
+				return new SurfacePositionMsg(es1090);
 
 			if ((ftc >= 9 && ftc <= 18) || (ftc >= 20 && ftc <= 22)) // airborne position message
-				return new AirbornePositionMsg(raw_message);
+				return new AirbornePositionMsg(es1090);
 
 			if (ftc == 19) { // possible velocity message, check subtype
 				int subtype = es1090.getMessage()[0]&0x7;
 
 				if (subtype == 1 || subtype == 2) // velocity over ground
-					return new VelocityOverGroundMsg(raw_message);
+					return new VelocityOverGroundMsg(es1090);
 				else if (subtype == 3 || subtype == 4) // airspeed & heading
-					return new AirspeedHeadingMsg(raw_message);
+					return new AirspeedHeadingMsg(es1090);
 			}
 
 			if (ftc == 28) { // aircraft status message, check subtype
 				int subtype = es1090.getMessage()[0]&0x7;
 
 				if (subtype == 1) // emergency/priority status
-					return new EmergencyOrPriorityStatusMsg(raw_message);
+					return new EmergencyOrPriorityStatusMsg(es1090);
 				if (subtype == 2) // TCAS resolution advisory report
-					return new TCASResolutionAdvisoryMsg(raw_message);
+					return new TCASResolutionAdvisoryMsg(es1090);
 			}
 
 			if (ftc == 31) { // operational status message
 				int subtype = es1090.getMessage()[0]&0x7;
 
 				if (subtype == 0 || subtype == 1) // airborne or surface?
-					return new OperationalStatusMsg(raw_message);
+					return new OperationalStatusMsg(es1090);
 			}
 
 			return es1090; // unknown extended squitter
 		}
 		else if (modes.getDownlinkFormat() == 20) {
-			return new CommBAltitudeReply(raw_message);
+			return new CommBAltitudeReply(modes);
 		}
 		else if (modes.getDownlinkFormat() == 21) {
-			return new CommBIdentifyReply(raw_message);
+			return new CommBIdentifyReply(modes);
 		}
 
 		return modes; // unknown mode s reply
