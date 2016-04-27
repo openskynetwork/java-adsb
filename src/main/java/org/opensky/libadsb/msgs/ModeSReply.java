eb2b09267e4519b4b86a0655dbde95c1ca506cd5
Matthias Schäfer <schaefer@cs.uni-kl.de>
2016-04-27 19:06:07 +0200

    Updates equals and hashCode

--- src/main/java/org/opensky/libadsb/msgs/ModeSReply.java
+++ src/main/java/org/opensky/libadsb/msgs/ModeSReply.java
@@ -284,6 +284,7 @@ public class ModeSReply implements Serializable {
 		
 		ModeSReply other = (ModeSReply)o;
 		return tools.areEqual(this.getPayload(), other.getPayload()) &&
+				tools.areEqual(this.getParity(), other.getParity()) &&
 				this.getDownlinkFormat() == other.getDownlinkFormat() &&
 				this.getFirstField() == other.getFirstField();
 	}
@@ -294,6 +295,8 @@ public class ModeSReply implements Serializable {
 		int sum = downlink_format<<3|first_field;
 		for (int i = 0; i<payload.length; ++i)
 			sum += payload[i]*31^(payload.length-i);
+		for (int i = 0; i<parity.length; ++i)
+			sum += parity[i]*31^(payload.length+parity.length-i);
 		return sum;
 	}
 }
